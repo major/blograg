e9d8368ceb510a914980e67fe89fb971ec08b2d3
Major Hayden <major@mhtx.net>
2025-04-21 11:40:03 -0500

    Add HNSW index

--- blograg/models.py
+++ blograg/models.py
@@ -1,5 +1,5 @@
 from pgvector.sqlalchemy import Vector  # type: ignore
-from sqlalchemy import String, create_engine, text
+from sqlalchemy import Index, String, create_engine, text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
 
 
@@ -17,6 +17,14 @@ class BlogPosts(Base):
     def __repr__(self) -> str:
         return f"<BlogPosts(id={self.id}, content={self.content})>"
 
+
+index = Index(
+    "hnsw_index_for_innerproduct_items_embedding",
+    BlogPosts.embedding,
+    postgresql_using="hnsw",
+    postgresql_with={"m": 16, "ef_construction": 64},
+    postgresql_ops={"embedding": "vector_ip_ops"},
+)
 
 engine = create_engine(
     "postgresql+psycopg2://postgres:secrete@127.0.0.1/ragdb", echo=False
